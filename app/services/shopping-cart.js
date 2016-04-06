919fde00a61f722a56fec9ad425120d6ee6811d5
Charlie Carter and Michelle Brecunier <michelle.brecunier@gmail.com>
2016-04-06 11:40:18 -0700

    create computed totalcost property on cart service

--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -8,5 +8,13 @@ export default Ember.Service.extend({
   },
   remove(item) {
     this.get('items').removeObject(item);
-  }
+  },
+
+  totalCost: Ember.computed('items.[]', function(){
+    var allCost = 0;
+    for (var i = 0; i < this.get('items.length'); i++) {
+      allCost += this.get('items')[i].get('cost');
+    }
+    return allCost;
+  })
 });
