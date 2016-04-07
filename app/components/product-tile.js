72a8bf697e97136c12d6b838203a9d7826091303
Charlie Carter and Michelle Brecunier <michelle.brecunier@gmail.com>
2016-04-07 10:43:13 -0700

    reorganize add to cart button as a component

--- app/components/product-tile.js
+++ app/components/product-tile.js
@@ -1,11 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  shoppingCart: Ember.inject.service(),
 
-  actions: {
-    addToCart(item) {
-      this.get('shoppingCart').add(item);
-    }
-  }
 });
